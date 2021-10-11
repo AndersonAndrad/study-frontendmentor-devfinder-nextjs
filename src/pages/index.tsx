55fbeb81fa7bf7604193edad134d0452f68ca50d
AndersonAndrad <anderson_andrade_@outlook.com>
2021-10-11 13:35:31 -0300

    documentation - adding images for documentation

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -61,6 +61,7 @@ export default function Home () {
       }
 
       setUserNotFound( false )
+      setUserName( '' )
       setUserInformation( userInformationFormatted )
 
     } ).catch( () => {
